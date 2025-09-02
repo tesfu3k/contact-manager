59ca00353158ff48485a451636fdbc989203ecbf
tesfu3k <tesfaye4r@gmail.com>
2025-09-02 09:53:28 +0300

    add get started link in home page

--- frontend/src/page/Home.jsx
+++ frontend/src/page/Home.jsx
@@ -1,5 +1,16 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Link
+        className="bg-blue-500 px-5 py-2 rounded-md hover:bg-blue-500/60 active:scale-90"
+        to="/sign-in"
+      >
+        Get Started
+      </Link>
+    </div>
+  );
 };
 
 export default Home;
